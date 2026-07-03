62982bebda520677bea62a4fa67d72a5c11862e6
devjohnnydev <johnnybraga2@gmail.com>
2026-07-03 19:20:41 -0300

    chore: remove view temporaria de seed

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from .seed_view import run_seeds_view
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('chat/', views.chat_view, name='chat'),
-    path('run-seeds-82736/', run_seeds_view, name='run_seeds'),
     path('admin/users/<int:user_pk>/role/', views.admin_change_role_view, name='admin_change_role'),
     path('admin/users/<int:user_pk>/toggle-active/', views.admin_toggle_active_view, name='admin_toggle_active'),
     path('admin/users/<int:user_pk>/delete/', views.admin_delete_user_view, name='admin_delete_user'),
