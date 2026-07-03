c8758751abe6ceb9885f674eff1bcb1996e47d12
devjohnnydev <johnnybraga2@gmail.com>
2026-07-03 19:26:15 -0300

    chore: remove view temporaria de transferencia

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from .fix_view import fix_view
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('chat/', views.chat_view, name='chat'),
-    path('fix-owner-4921/', fix_view, name='fix_view'),
     path('admin/users/<int:user_pk>/role/', views.admin_change_role_view, name='admin_change_role'),
     path('admin/users/<int:user_pk>/toggle-active/', views.admin_toggle_active_view, name='admin_toggle_active'),
     path('admin/users/<int:user_pk>/delete/', views.admin_delete_user_view, name='admin_delete_user'),
